4458bdaf976b6e20aebc7095cc8a8e8696799401
charley <charley.mckay.2023@uni.strath.ac.uk>
2025-11-21 14:02:24 +0000

    Added in beginning of ClientHandler.java contents.

--- main/src/ClientHandler.java
+++ main/src/ClientHandler.java
@@ -1,2 +1,34 @@
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+import java.util.Set;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+
+    private final Socket socket;
+    private final BankService bankService;
+    private final Set<String> activeUsers;
+
+    public ClientHandler(Socket socket, BankService bankService, Set<String> activeUsers) {
+        this.socket = socket;
+        this.bankService = bankService;
+        this.activeUsers = activeUsers;
+    }
+
+
+    @Override
+    public void run() {
+        String currentUser = null;
+
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
 }
